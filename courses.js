45dfba679b31cd56be6e0acc391a3fb98864794c
Al Shahriar Niloy <shahriar_niloy@outlook.com>
2019-06-29 09:34:16 +0600

    Fixed Course Module for SQL Injection Prevention

--- courses.js
+++ courses.js
@@ -19,14 +19,14 @@ router.get("", (req, res) => {
 
 router.post("", auth, (req, res) => {
   const { code, title, timeslot, section, picture } = req.body;
-  conn.query(`INSERT INTO COURSES ( CODE, TITLE, SEC, TIMESLOT, PIC ) VALUES ('${code}', '${title}', '${section}', '${timeslot}', '${picture}')`, (err, result, field) => {
+  conn.query(`INSERT INTO COURSES ( CODE, TITLE, SEC, TIMESLOT, PIC ) VALUES (?,?,?,?,?)`, [code, title, section, timeslot, picture], (err, result, field) => {
     if(err) throw err;
     res.status(200).send(result);
   });
 });
 
 router.get("/:id", (req, res) => {
-  conn.query(`SELECT * FROM courses where id = ${req.params.id}`, function(err, result, fields) {
+  conn.query(`SELECT * FROM courses where id = ?`, [req.params.id], function(err, result, fields) {
     if (err) throw err;
     console.log(result);
     res.send(result);
@@ -34,7 +34,7 @@ router.get("/:id", (req, res) => {
 });
 
 router.delete("/:id", auth, (req, res) => {
-  conn.query(`DELETE FROM courses where id = ${req.params.id}`, function(err, result, fields) {
+  conn.query(`DELETE FROM courses where id = ?`, [req.params.id], function(err, result, fields) {
     if (err) {
       res.status(401).send(result);
       throw err;
@@ -46,7 +46,7 @@ router.delete("/:id", auth, (req, res) => {
 router.put("/:id", auth, (req, res) => {
   const { code, timeslot, title, sec } = req.body;
   console.log("code: ", req.body);
-  conn.query(`UPDATE COURSES SET code = '${code}', timeslot = '${timeslot}', sec = ${sec}, title = '${title}' where id = ${req.params.id}`, function(err, result, fields) {
+  conn.query(`UPDATE COURSES SET code = ?, timeslot = ?, sec = ?, title = ? where id = ?`, [code, timeslot, sec, title, req.params.id], function(err, result, fields) {
     if (err) {
       res.status(401).send(error);
       throw err;
@@ -56,7 +56,7 @@ router.put("/:id", auth, (req, res) => {
 });
 
 router.get("/materials/:id", (req, res) => {
-  conn.query(`SELECT courseID, name, link FROM MATERIAL WHERE COURSEID = ${req.params.id}`, (err, result, fields) => {
+  conn.query(`SELECT courseID, name, link FROM MATERIAL WHERE COURSEID = ?`, [req.params.id], (err, result, fields) => {
     if(err) throw err;
     //console.log(result)
     if(result)
@@ -69,7 +69,7 @@ router.get("/materials/:id", (req, res) => {
 router.put("/materials/:id", auth, (req, res) => {
   const material = req.body;
   let err = false;
-  conn.query(`delete from material where courseID = ${req.params.id}`, (error, result, fields) => {
+  conn.query(`delete from material where courseID = ?`, [req.params.id], (error, result, fields) => {
     if(error) {
       err = true;
       throw error; 
@@ -77,7 +77,7 @@ router.put("/materials/:id", auth, (req, res) => {
   });
   material.map((row) => {
     conn.query(`INSERT INTO material (courseID, name, link)
-                VALUES (${req.params.id}, '${row.name}', '${row.link}')`, (error, result, fields) => {
+                VALUES (?,?,?)`, [req.params.id, row.name, row.link], (error, result, fields) => {
       if(error) {
         throw error; 
         err = true;
@@ -89,7 +89,7 @@ router.put("/materials/:id", auth, (req, res) => {
 router.delete("/materials/delete/:courseID/:name/", auth, (req, res) => {
   const { courseID, name } = req.params;
   console.log(courseID, name);
-  conn.query(`DELETE FROM MATERIAL WHERE COURSEID = ${courseID} AND NAME = '${name}'`, (error, result, fields) => {
+  conn.query(`DELETE FROM MATERIAL WHERE COURSEID = ? AND NAME = ?`, [courseID, name], (error, result, fields) => {
     if(error) throw error;
     res.status(200).send({});
   });
